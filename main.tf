c21802bdc49e6800a10ec98ac86abc316246e09b
Yordan Halachev <yordan.i.halachev@gmail.com>
2020-02-20 13:26:41 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,11 +11,19 @@ provider "google" {
 resource "google_compute_instance" "metabase" {
   name         = "random-name-changed"
   machine_type = "n1-standard-1"
+  
+  tags = ["foo", "bar"]
+  
+  metadata = {
+    foo = "bar"
+  }
+  
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-9"
     }
   }
+  
   network_interface {
     network = "default"
     access_config {
